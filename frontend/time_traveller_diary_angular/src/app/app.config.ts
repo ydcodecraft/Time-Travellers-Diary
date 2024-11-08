302d619e80114098aeed7f034a82b32d568fa69b
ydcodecraft@gmail.com <ydcodecraft@gmail.com>
2024-11-08 16:18:11 -0700

    using environment variables in angular

--- frontend/time_traveller_diary_angular/src/app/app.config.ts
+++ frontend/time_traveller_diary_angular/src/app/app.config.ts
@@ -5,34 +5,34 @@ import { AuthHttpInterceptor, authHttpInterceptorFn, provideAuth0 } from '@auth0
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { environment } from '../environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideAuth0({
-      domain: 'dev-ydcodecraft.ca.auth0.com',
-      clientId: 'BAWgx55vPLRKjhPM6lgldLqT9r3kWAPZ',
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
       authorizationParams: {
-        // redirect_uri: window.location.origin,
-        redirect_uri: 'http://localhost:4200',
-        audience: 'https://dev-ydcodecraft.ca.auth0.com/api/v2/',
+        redirect_uri: window.location.origin,
+        audience: environment.auth0.audience,
       },
       httpInterceptor: {
         allowedList: [
+          // {
+          //   uri: 'https://dev-ydcodecraft.ca.auth0.com/api/v2/*',
+          //   tokenOptions: {
+          //     authorizationParams: {
+          //       audience: 'https://dev-ydcodecraft.ca.auth0.com/api/v2/',
+          //     }
+          //   }
+          // },
           {
-            uri: 'https://dev-ydcodecraft.ca.auth0.com/api/v2/*',
+            uri: `${environment.backend.uri}/*`,
             tokenOptions: {
               authorizationParams: {
-                audience: 'https://dev-ydcodecraft.ca.auth0.com/api/v2/',
-              }
-            }
-          },
-          {
-            uri: 'http://127.0.0.1:8000/*',
-            tokenOptions: {
-              authorizationParams: {
-                audience: 'https://dev-ydcodecraft.ca.auth0.com/api/v2/',
+                audience: environment.auth0.audience,
               }
             }
           }
---
+++ frontend/time_traveller_diary_angular/src/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    auth0: {
+        domain: 'dev-ydcodecraft.ca.auth0.com',
+        clientId: 'BAWgx55vPLRKjhPM6lgldLqT9r3kWAPZ',
+        redirect_uri: 'http://localhost:4200',
+        audience: 'https://dev-ydcodecraft.ca.auth0.com/api/v2/',
+    },
+    backend: {
+        uri: 'http://127.0.0.1:8000',
+    }
+    
+}
