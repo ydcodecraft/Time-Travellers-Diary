600c80f61d3229ac149ca1a258c2ce0b7776fdd3
ydcodecraft@gmail.com <ydcodecraft@gmail.com>
2024-11-10 22:27:48 -0700

    update diary card and diary page to use the latest sdk

--- frontend/time_traveller_diary_angular/src/app/components/diary-card/diary-card.component.ts
+++ frontend/time_traveller_diary_angular/src/app/components/diary-card/diary-card.component.ts
@@ -1,21 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { Diary, DiaryEntry } from '@ydcodecraft/time_travellers_diary_api';
 
 @Component({
   selector: 'app-diary-card',
   standalone: true,
-  imports: [MatCardModule, MatChipsModule],
+  imports: [CommonModule, MatCardModule, MatChipsModule],
   templateUrl: './diary-card.component.html',
   styleUrl: './diary-card.component.scss'
 })
 export class DiaryCardComponent implements OnInit{
   // TODO: placeholder, change this to dto soon
-  @Input() diaryData: any;
+  @Input() diaryData!: Diary;
   
   
   ngOnInit(): void {
     console.log(this.diaryData);
+  }
+
+
+  get latestDiaryEntry(): DiaryEntry | null {
+    if (this.diaryData.diary_entries?.length === undefined || this.diaryData.diary_entries?.length === null ||this.diaryData.diary_entries?.length === 0){
+      return null;
+    }
+    return this.diaryData.diary_entries[this.diaryData.diary_entries.length- 1];
   }
 }
 
--- frontend/time_traveller_diary_angular/src/app/pages/diary/diary.component.ts
+++ frontend/time_traveller_diary_angular/src/app/pages/diary/diary.component.ts
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { DiaryService, Diary } from '@ydcodecraft/time_travellers_diary_api';
 
 @Component({
   selector: 'app-diary',
@@ -15,117 +16,125 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './diary.component.scss'
 })
 export class DiaryComponent implements OnInit{
+  public diaryData: Diary[] = [];
+
+
   constructor(
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    public diaryService: DiaryService
   ) {}
 
-  public diaryData: any;
-  
 
   ngOnInit(): void {
-    const mockDiaryData = [
-      {
-        id: 1,
-        date: "2024-06-15",
-        entries: [
-          {
-            timePeriod: { id: 1, value: "Morning" },
-            description: "Spent the morning at the beach with friends. The weather was perfect!",
-            mood: { id: 1, value: "happy" }
-          },
-          {
-            timePeriod: { id: 2, value: "Afternoon" },
-            description: "Had lunch at a beachside cafe. Delicious seafood!",
-            mood: { id: 2, value: "satisfied" }
-          }
-        ]
-      },
-      {
-        id: 2,
-        date: "2024-06-16",
-        entries: [
-          {
-            timePeriod: { id: 3, value: "Evening" },
-            description: "Tried cooking a new pasta recipe. It was a success!",
-            mood: { id: 2, value: "satisfied" }
-          }
-        ]
-      },
-      {
-        id: 3,
-        date: "2024-06-17",
-        entries: [
-          {
-            timePeriod: { id: 4, value: "All Day" },
-            description: "It rained all day today. I stayed in and read a book.",
-            mood: { id: 3, value: "reflective" }
-          }
-        ]
-      },
-      {
-        id: 4,
-        date: "2024-06-18",
-        entries: [
-          {
-            timePeriod: { id: 1, value: "Morning" },
-            description: "Completed my workout goals at the gym.",
-            mood: { id: 4, value: "motivated" }
-          }
-        ]
-      },
-      {
-        id: 5,
-        date: "2024-06-19",
-        entries: [
-          {
-            timePeriod: { id: 3, value: "Evening" },
-            description: "Watched a movie with my family. We laughed so much!",
-            mood: { id: 5, value: "joyful" }
-          }
-        ]
-      },
-      {
-        id: 6,
-        date: "2024-06-20",
-        entries: [
-          {
-            timePeriod: { id: 5, value: "Weekend" },
-            description: "Went for a weekend getaway to the mountains.",
-            mood: { id: 6, value: "content" }
-          }
-        ]
-      },
-      {
-        id: 7,
-        date: "2024-06-21",
-        entries: [
-          {
-            timePeriod: { id: 2, value: "Afternoon" },
-            description: "Felt anxious about an upcoming presentation.",
-            mood: { id: 7, value: "anxious" }
-          }
-        ]
-      },
-      {
-        id: 8,
-        date: "2024-06-22",
-        entries: [
-          {
-            timePeriod: { id: 3, value: "Evening" },
-            description: "Had a deep conversation with a friend.",
-            mood: { id: 8, value: "grateful" }
-          }
-        ]
-      }
-    ];
+    this.fetchDiaries();
+
+    // const mockDiaryData = [
+    //   {
+    //     id: 1,
+    //     date: "2024-06-15",
+    //     entries: [
+    //       {
+    //         timePeriod: { id: 1, value: "Morning" },
+    //         description: "Spent the morning at the beach with friends. The weather was perfect!",
+    //         mood: { id: 1, value: "happy" }
+    //       },
+    //       {
+    //         timePeriod: { id: 2, value: "Afternoon" },
+    //         description: "Had lunch at a beachside cafe. Delicious seafood!",
+    //         mood: { id: 2, value: "satisfied" }
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     id: 2,
+    //     date: "2024-06-16",
+    //     entries: [
+    //       {
+    //         timePeriod: { id: 3, value: "Evening" },
+    //         description: "Tried cooking a new pasta recipe. It was a success!",
+    //         mood: { id: 2, value: "satisfied" }
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     id: 3,
+    //     date: "2024-06-17",
+    //     entries: [
+    //       {
+    //         timePeriod: { id: 4, value: "All Day" },
+    //         description: "It rained all day today. I stayed in and read a book.",
+    //         mood: { id: 3, value: "reflective" }
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     id: 4,
+    //     date: "2024-06-18",
+    //     entries: [
+    //       {
+    //         timePeriod: { id: 1, value: "Morning" },
+    //         description: "Completed my workout goals at the gym.",
+    //         mood: { id: 4, value: "motivated" }
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     id: 5,
+    //     date: "2024-06-19",
+    //     entries: [
+    //       {
+    //         timePeriod: { id: 3, value: "Evening" },
+    //         description: "Watched a movie with my family. We laughed so much!",
+    //         mood: { id: 5, value: "joyful" }
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     id: 6,
+    //     date: "2024-06-20",
+    //     entries: [
+    //       {
+    //         timePeriod: { id: 5, value: "Weekend" },
+    //         description: "Went for a weekend getaway to the mountains.",
+    //         mood: { id: 6, value: "content" }
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     id: 7,
+    //     date: "2024-06-21",
+    //     entries: [
+    //       {
+    //         timePeriod: { id: 2, value: "Afternoon" },
+    //         description: "Felt anxious about an upcoming presentation.",
+    //         mood: { id: 7, value: "anxious" }
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     id: 8,
+    //     date: "2024-06-22",
+    //     entries: [
+    //       {
+    //         timePeriod: { id: 3, value: "Evening" },
+    //         description: "Had a deep conversation with a friend.",
+    //         mood: { id: 8, value: "grateful" }
+    //       }
+    //     ]
+    //   }
+    // ];
     
-    this.diaryData = mockDiaryData;
+    // this.diaryData = mockDiaryData;
 
 
   }
 
   // return a list of diary dtos
   private fetchDiaries(): void {
+    this.diaryService.diaryList().subscribe((diaryData) => {
+      this.diaryData = diaryData;
+    })
+
     
   }
 
