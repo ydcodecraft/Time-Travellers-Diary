8e48b3d2e4922caa279084cb42c84e080af5c8d7
ydcodecraft@gmail.com <ydcodecraft@gmail.com>
2024-11-10 22:27:21 -0700

    updated routing for time traveller creation

--- frontend/time_traveller_diary_angular/src/app/app.routes.ts
+++ frontend/time_traveller_diary_angular/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '@auth0/auth0-angular';
 import { HomeComponent } from './pages/home/home/home.component';
 import { DiaryComponent } from './pages/diary/diary.component';
+import { TimeTravellerCreationComponent } from './pages/time-traveller-creation/time-traveller-creation.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
-    { path: 'diary-dashboard', component: DiaryComponent, canActivate: [AuthGuard]}
+    { path: 'time-traveller-creation', component: TimeTravellerCreationComponent, canActivate: [AuthGuard]},
+    { path: 'diary-dashboard', component: DiaryComponent, canActivate: [AuthGuard]},
 ];
 
 
