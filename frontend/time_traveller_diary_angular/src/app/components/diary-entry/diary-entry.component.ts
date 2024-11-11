3a2011850fc4ef10ab0a76ab7edcbbf9b44d8652
ydcodecraft@gmail.com <ydcodecraft@gmail.com>
2024-11-11 03:24:37 -0700

    diary detail improvement force diary dashboard refresh after update

--- frontend/time_traveller_diary_angular/src/app/components/diary-entry/diary-entry.component.ts
+++ frontend/time_traveller_diary_angular/src/app/components/diary-entry/diary-entry.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, inject, Inject, OnInit, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Form, FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -9,106 +9,113 @@ import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
+import { DiaryService, TimePeriodService, TimePeriod, MoodService, Mood, Diary, DiaryEntryService, DiaryEntryUpdateCreate, DiaryEntry, PatchedDiary, DiaryCreate, PatchedDiaryEntryUpdateCreate } from '@ydcodecraft/time_travellers_diary_api'
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 @Component({
   selector: 'app-diary-entry',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, ReactiveFormsModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatInputModule, MatSelectModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, ReactiveFormsModule, MatFormFieldModule, MatDatepickerModule, 
+    MatNativeDateModule, MatInputModule, MatSelectModule, MatIconModule, MatSnackBarModule],
   templateUrl: './diary-entry.component.html',
   styleUrl: './diary-entry.component.scss'
 })
 export class DiaryEntryComponent implements OnInit{
-  private readonly dialogRef = inject(MatDialogRef<DiaryEntryComponent>)
+  private readonly dialogRef = inject(MatDialogRef<DiaryEntryComponent>);
+  private readonly snackBar = inject(MatSnackBar);
+
   public form: FormGroup;
-  public timePeriodSelection: any;
-  public moodSelection: any;
-  
-  // placeholder for now
-  public timePeriods = [
-    { 
-      id: 1,
-      value: "viking"
-    }, 
-    {
-      id: 2,
-      value: "bronze age"
-    }, 
-    {
-      id: 3,
-      value: "medieval"
-    }, 
-    {
-      id: 4,
-      value: "modern time"
-    },
-    {
-      id: 5,
-      value: "Afternoon"
-    }
-  ];
 
-
-  public moods: any;
+  public timePeriods: TimePeriod[] = [];
+  public moods: Mood[] = [];
 
   constructor(
     // TODO: update the diaryData to the actual object type
-    @Inject(MAT_DIALOG_DATA) public diaryData: any,
-    private formBuilder: FormBuilder) {
+    @Inject(MAT_DIALOG_DATA) public diaryData: Diary,
+    private formBuilder: FormBuilder,
+    private diaryService: DiaryService,
+    private diaryEntryService: DiaryEntryService,
+    private timePeriodService: TimePeriodService,
+    private moodService: MoodService) {
     this.form = this.formBuilder.group({
+      id: [''],
       date: ['', Validators.required],
       diaryEntries: this.formBuilder.array([])
     })
   }
-
+  
   // helper function to access diaryEntries quickly
-  get diaryEntries(): FormArray {
+  get diaryEntriesFormArray(): FormArray {
     return this.form.get('diaryEntries') as FormArray;
   }
 
   ngOnInit(): void {
-    this.addDiaryEntry();
-
-
-    if (this.diaryData?.date) {
-      this.form.get('date')?.setValue(this.diaryData.date);
-    }
-    
-    console.log(this.diaryData);
-    console.log(this.diaryData?.entries);
-
-    if (this.diaryData?.entries) {
-      for (let entry of this.diaryData?.entries) {
-        console.log(entry.timePeriod);
-        console.log(entry.mood);
-        const entryFormGroup = this.formBuilder.group({
-          timePeriod: [entry.timePeriod.id, Validators.required],
-          mood: [entry.mood.id, Validators.required],
-          description : [entry.description, Validators.required],
-        })
-        this.diaryEntries?.push(entryFormGroup);
-      }
-      // this.diaryEntries?.push(this.diaryData.entries);
-    }
-
-    console.log(this.diaryEntries);
-
-    // this.form.get('diaryEntries')?.setValue(this.diaryData.diaryEntries);
-    // console.log(this.diaryEntries);
-    // if (!this.diaryData) {
-    //   this.addDiaryEntry();
-    // }
+    this.loadData();
   }
 
+
+  private loadData(): void{
+
+    this.loadDropdownSelectionData();
+    this.loadDiaryEntryFormData();
+
+  }
+  
+  // load dropdown/ selection option data
+  private loadDropdownSelectionData(): void {
+    // load time period listing
+    this.timePeriodService.timePeriodList().subscribe((timePeriods) => {
+      this.timePeriods = timePeriods;
+    })
+
+    // load mood listing
+    this.moodService.moodList().subscribe((moods) => {
+      this.moods = moods;
+    })
+  }
+
+  // load the input data and map them to the ag form
+  // dynamically generate any component if needed
+  private loadDiaryEntryFormData(): void {
+    if (this.diaryData?.id) {
+      this.form.get('id')?.setValue(this.diaryData.id , { emitEvent: false });
+    }
+    if (this.diaryData?.date) {
+      this.form.get('date')?.setValue(this.diaryData.date, { emitEvent: false });
+    }
+    // becasue a diary can contain multiple diary entries
+    // iterate through the diary entries array and generate n number of diary entry formGroup
+    // then add each diary entry formGroup to the formArray
+    
+    if (this.diaryData?.diary_entries) {
+      for (let entry of this.diaryData?.diary_entries) {
+        const entryFormGroup = this.formBuilder.group({
+          id: [entry.id, Validators.required],
+          time_period: [entry.time_period?.id, Validators.required],
+          mood: [entry.mood?.id, Validators.required],
+          description : [entry.description, Validators.required],
+        });
+
+        this.diaryEntriesFormArray?.push(entryFormGroup);
+      }
+    }
+    else {
+      this.addDiaryEntry();
+    }
+  }
+
+  
+  // helper function to add an empty diary entry
   addDiaryEntry(): void {
     const emptyDiaryEntry = this.formBuilder.group({
-      timePeriod: ['', Validators.required],
+      id: [''],
+      time_period: ['', Validators.required],
       mood: ['', Validators.required],
       description : ['', Validators.required],
     });
-
-    this.diaryEntries.push(emptyDiaryEntry);
-
+    this.diaryEntriesFormArray.push(emptyDiaryEntry);
+    console.log(emptyDiaryEntry);
   }
 
   onClose(): void {
@@ -116,6 +123,82 @@ export class DiaryEntryComponent implements OnInit{
   }
 
   onSubmit(): void {
-    throw('not implemented yet')
+    console.log(this.diaryEntriesFormArray);
+
+    if (this.diaryEntriesFormArray.dirty){
+        // if a diary does not have an id, then it's a newly created entry. Create it
+        if (this.form.get('id')?.value === '') {
+        // leave diary_entries null for now
+        // insert all diary entries later
+        // we can also create all diary entries and issue a single http request
+        const newDiary: DiaryCreate = {
+          date: this.form.get('date')?.value,
+          diary_entries: null
+        };
+        this.diaryService.diaryCreate(newDiary);
+      }
+        // if a diary has an id, then it's an existing entry. Update it
+        else {
+        const updatedDiary: PatchedDiary = {
+          date: this.form.get('date')?.value
+        }
+
+        this.diaryService.diaryPartialUpdate(this.form.get('id')?.value, updatedDiary).subscribe({
+          next: (result) => { console.log(result);},
+          error: (err) => {
+            this.snackBar.open("Oops, we have some issue saving your adventure! Please try again later!", "I'll Try Again Later!");
+            console.error(err);
+          }
+        });
+      }
+        
+      // iterate through all the diary entries and check if they are dirty and valid
+      // submit any dirty diary entries
+      for (let entryForm of this.diaryEntriesFormArray.controls) {
+        if (entryForm.dirty) {
+          // if a diary entry does not have an id, then it's a newly created entry. Create it
+          if (entryForm.get('id')?.value === "") {
+            const newDiaryEntry: DiaryEntryUpdateCreate = {
+              diary: this.form.get('id')?.value,
+              description: entryForm.get('description')?.value,
+              mood: entryForm.get('mood')?.value,
+              time_period: entryForm.get('time_period')?.value
+            };
+
+            this.diaryEntryService.diaryEntryCreate(newDiaryEntry).subscribe({
+              next: (result) => { 
+                console.log(result);
+                this.dialogRef.close('refresh');
+              },
+              error: (err) => {
+                this.snackBar.open("Oops, we have some issue saving your adventure! Please try again later!", "I'll Try Again Later!");
+                console.error(err);
+              }
+            });
+          }
+          // if a diary entry has an id, then it's an existing entry. Update it
+          else {
+            const updatedDiaryEntry: PatchedDiaryEntryUpdateCreate = {
+              description: entryForm.get('description')?.value,
+              mood: entryForm.get('mood')?.value,
+              time_period: entryForm.get('time_period')?.value
+            };
+            console.log(entryForm.get('id')?.value);
+            console.log(updatedDiaryEntry);
+            this.diaryEntryService.diaryEntryPartialUpdate(entryForm.get('id')?.value, updatedDiaryEntry).subscribe({
+              next: (result) => { 
+                this.dialogRef.close('refresh');
+                console.log(result);
+              },
+              error: (err) => {
+                this.snackBar.open("Oops, we have some issue saving your adventure! Please try again later!", "I'll Try Again Later!");
+                console.error(err);
+              }
+            })
+          }
+          console.log(entryForm);
+        }
+      }
+    }
   }
 }
--- frontend/time_traveller_diary_angular/src/app/pages/diary/diary.component.ts
+++ frontend/time_traveller_diary_angular/src/app/pages/diary/diary.component.ts
@@ -27,106 +27,6 @@ export class DiaryComponent implements OnInit{
 
   ngOnInit(): void {
     this.fetchDiaries();
-
-    // const mockDiaryData = [
-    //   {
-    //     id: 1,
-    //     date: "2024-06-15",
-    //     entries: [
-    //       {
-    //         timePeriod: { id: 1, value: "Morning" },
-    //         description: "Spent the morning at the beach with friends. The weather was perfect!",
-    //         mood: { id: 1, value: "happy" }
-    //       },
-    //       {
-    //         timePeriod: { id: 2, value: "Afternoon" },
-    //         description: "Had lunch at a beachside cafe. Delicious seafood!",
-    //         mood: { id: 2, value: "satisfied" }
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     id: 2,
-    //     date: "2024-06-16",
-    //     entries: [
-    //       {
-    //         timePeriod: { id: 3, value: "Evening" },
-    //         description: "Tried cooking a new pasta recipe. It was a success!",
-    //         mood: { id: 2, value: "satisfied" }
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     id: 3,
-    //     date: "2024-06-17",
-    //     entries: [
-    //       {
-    //         timePeriod: { id: 4, value: "All Day" },
-    //         description: "It rained all day today. I stayed in and read a book.",
-    //         mood: { id: 3, value: "reflective" }
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     id: 4,
-    //     date: "2024-06-18",
-    //     entries: [
-    //       {
-    //         timePeriod: { id: 1, value: "Morning" },
-    //         description: "Completed my workout goals at the gym.",
-    //         mood: { id: 4, value: "motivated" }
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     id: 5,
-    //     date: "2024-06-19",
-    //     entries: [
-    //       {
-    //         timePeriod: { id: 3, value: "Evening" },
-    //         description: "Watched a movie with my family. We laughed so much!",
-    //         mood: { id: 5, value: "joyful" }
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     id: 6,
-    //     date: "2024-06-20",
-    //     entries: [
-    //       {
-    //         timePeriod: { id: 5, value: "Weekend" },
-    //         description: "Went for a weekend getaway to the mountains.",
-    //         mood: { id: 6, value: "content" }
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     id: 7,
-    //     date: "2024-06-21",
-    //     entries: [
-    //       {
-    //         timePeriod: { id: 2, value: "Afternoon" },
-    //         description: "Felt anxious about an upcoming presentation.",
-    //         mood: { id: 7, value: "anxious" }
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     id: 8,
-    //     date: "2024-06-22",
-    //     entries: [
-    //       {
-    //         timePeriod: { id: 3, value: "Evening" },
-    //         description: "Had a deep conversation with a friend.",
-    //         mood: { id: 8, value: "grateful" }
-    //       }
-    //     ]
-    //   }
-    // ];
-    
-    // this.diaryData = mockDiaryData;
-
-
   }
 
   // return a list of diary dtos
@@ -142,6 +42,13 @@ export class DiaryComponent implements OnInit{
     const dialogRef = this.dialog.open(DiaryEntryComponent, {
       data: diaryEntry,
       minWidth: '60vw',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('Dialog closed');
+      if (result === 'refresh') {
+        this.fetchDiaries();
+      }
     });
   }
 }
